32649dc435f90fabb3c8b9a37815bfe44b97e76a
karamakcsc <karamakcsc@hotmail.com>
2024-10-21 12:08:49 +0300

    update

--- masar_wpaintech/custom/purchase_order/purchase_order.js
+++ masar_wpaintech/custom/purchase_order/purchase_order.js
@@ -1,50 +1,49 @@
-/////////////Calculation Fields/////////////////Start Code
+///////////// Calculation Fields /////////////////
 
 frappe.ui.form.on("Purchase Order Item", {
     qty: function(frm, cdt, cdn) {
-        calc_rate_per_unit(frm, cdt, cdn);
         calc_carton_capacity(frm, cdt, cdn);
+        calc_rate_per_unit(frm, cdt, cdn);
     },
     rate: function(frm, cdt, cdn) {
-        calc_rate_per_unit(frm, cdt, cdn);
         calc_carton_capacity(frm, cdt, cdn);
+        calc_rate_per_unit(frm, cdt, cdn);
     },
     uom: function(frm, cdt, cdn) {
-        calc_rate_per_unit(frm, cdt, cdn);
         calc_carton_capacity(frm, cdt, cdn);
+        calc_rate_per_unit(frm, cdt, cdn);
     }
-  });
-  
-  function calc_rate_per_unit(frm, cdt, cdn) {
+});
+
+function calc_rate_per_unit(frm, cdt, cdn) {
     var d = locals[cdt][cdn];
     if (d.item_code) {
         var uomNumericValue;
         if (d.uom.toLowerCase() === "lt" || d.uom.toLowerCase() === "kg" || d.uom.toLowerCase() === "gallon") {
             uomNumericValue = 1; // Set a default value for "LT" if needed
         } else {
-            uomNumericValue = parseFloat(d.uom.match(/\d+(\.\d+)?/)[0]);
+            var match = d.uom.match(/\d+(\.\d+)?/);
+            uomNumericValue = match ? parseFloat(match[0]) : NaN;
         }
-  
+
         if (!isNaN(uomNumericValue)) {
             d.custom_rate_per_unit = flt(d.rate / uomNumericValue);
-            cur_frm.refresh_field("custom_rate_per_unit");
+            frm.refresh_field("items");
         } else {
-            frappe.msgprint(__("Invalid numeric value in UOM"));
+            frappe.msgprint(__("The UOM is not numerical"));
         }
     }
-  }
-  
-  function calc_carton_capacity(frm, cdt, cdn) {
+}
+
+function calc_carton_capacity(frm, cdt, cdn) {
     var d = locals[cdt][cdn];
     if (d.item_code && d.custom_carton_capacity) {
         d.custom_no_cartron = flt(d.qty / d.custom_carton_capacity);
-        cur_frm.refresh_field("custom_no_cartron");
+        frm.refresh_field("items");
     }
-  }
-  
-/////////////Calculation Fields/////////////////End Code
-  
-/////////////Fetching Data/////////////////Start Code
+}
+
+///////////// Fetching Data /////////////////
 
 frappe.ui.form.on('Purchase Order', {
     refresh: function(frm) {
@@ -52,8 +51,22 @@ frappe.ui.form.on('Purchase Order', {
             var doc = grid_row.doc;
             fetch_carton_capacity(frm, doc);
         });
-    }
+    },
+
+    // supplier: function(frm) {
+    //     if (frm.doc.supplier) {
+    //         frm.fields_dict.items.grid.get_field('item_code').get_query = function(doc, cdt, cdn) {
+    //             return {
+    //                 filters: {
+    //                     'supplier': frm.doc.supplier
+    //                 }
+    //             };
+    //         };
+    //     }
+    // }
 });
+
+
 
 frappe.ui.form.on('Purchase Order Item', {
     item_code: function(frm, cdt, cdn) {
@@ -74,15 +87,14 @@ function fetch_carton_capacity(frm, doc) {
             callback: function(r) {
                 if (r.message && r.message.custom_carton_capacity) {
                     frappe.model.set_value(doc.doctype, doc.name, 'custom_carton_capacity', r.message.custom_carton_capacity);
+                    calc_carton_capacity(frm, doc.doctype, doc.name);
                 }
             }
         });
     }
 }
 
-/////////////Fetching Data/////////////////End Code
-
-////////////Filter Fields//////////////////Start Code
+//////////// Filter Fields //////////////////
 
 frappe.ui.form.on('Purchase Order', {
     onload: function(frm) {
@@ -98,7 +110,6 @@ frappe.ui.form.on('Purchase Order', {
         shipping_terms_filter(frm);
     }
 });
-
 
 function freight_type_filter(frm) {
     frm.fields_dict['custom_freight_type'].get_query = function() {
@@ -119,5 +130,3 @@ function shipping_terms_filter(frm) {
         };
     };
 }
-
-////////////Filter Fields////////////////////End Code
