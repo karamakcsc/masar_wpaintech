0c38a6e8df1ef51ab16d14b649f5e4aaccf4cfbf
karamakcsc <karamakcsc@hotmail.com>
2025-05-13 17:09:59 +0300

    update

--- masar_wpaintech/masar_wpaintech/report/item_stock_balance/item_stock_balance.py
+++ masar_wpaintech/masar_wpaintech/report/item_stock_balance/item_stock_balance.py
@@ -15,14 +15,17 @@ def data(filters):
         conditions += f" AND tb.warehouse = '{filters.get('warehouse')}'"
     if filters.get("brand"):
         conditions += f" AND ti.brand = '{filters.get('brand')}'"
-        
+
+    select = "tb.warehouse,"
+    group_by = "tb.item_code, tb.warehouse"
+
     if filters.get("group_by"):
-        group_by_fiter = filters.get("group_by")
-        if group_by_fiter == "Warehouse":
+        group_by_filter = filters.get("group_by")
+        if group_by_filter == "Warehouse":
             select = "tb.warehouse,"
             group_by = "tb.item_code, tb.warehouse"
-        elif group_by_fiter == "Item":
-            select = "All Warehouses,"
+        elif group_by_filter == "Item":
+            select = "'All Warehouses' as warehouse,"
             group_by = "tb.item_code"
             
     sql = frappe.db.sql(f"""
@@ -41,6 +44,7 @@ def data(filters):
 			ti.custom_manufacturing_code AS `Manufacturing Code`,
 			ti.brand AS `Brand`,
 			tb.stock_uom AS `Stock UOM`,
+			ti.custom_carton_capacity AS `Qty Per Carton`,
 			{select}
 			SUM(tb.actual_qty) AS `Stock Balance`,
 			AVG(tb.valuation_rate) AS `Valuation Rate`,
